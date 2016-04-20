00451c098853363559a185ab7f266647621a5f02
Andreas Born <futur.andy@googlemail.com>
2016-04-20 12:01:05 +0200

    Open source code link in a new window

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,1 +1,1 @@
-<p>Page rendered in <strong>{elapsed_time}</strong> seconds.<br />&copy; 2016 Andreas Born<br /><a href="https://github.com/Phoosha/dienstplan">Source code on GitHub</a></p>
+<p>Page rendered in <strong>{elapsed_time}</strong> seconds.<br />&copy; 2016 Andreas Born<br /><a href="https://github.com/Phoosha/dienstplan" target="_blank">Source code on GitHub</a></p>
